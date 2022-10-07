4f843ceeb25a3a1d8add93214eb7ac7e86ea0fa8
zahidalidev <m.zahidalidev@gmail.com>
2022-10-08 02:19:08 +0500

    bug in expo-firestore-offline-persistence

--- app/api/categories.js
+++ app/api/categories.js
@@ -3,12 +3,17 @@ import { firebaseFirestore } from '.'
 
 import { sort } from '../utils/helpers'
 
+
+// Firestore.enablePersistence()
+//   .then((data) => console.log('Offline successfully enable: ', data))
+//   .catch((error) => console.log('Offline error: ', error))
+
 export const fetchAllCategories = async () => {
   const snapshot = await getDocs(collection(firebaseFirestore, 'categories'))
   return snapshot.docs.map((doc) => doc.data())
 }
 
-export const fetchSubCategories = async id => {
+export const fetchSubCategories = async (id) => {
   const q = query(collection(firebaseFirestore, 'subCategories'), where('quiz_id', '==', id))
   const snapshot = await getDocs(q)
   return sort(snapshot.docs)
@@ -20,7 +25,7 @@ export const fetchAllSubCategories = async () => {
   return sort(snapshot.docs)
 }
 
-export const fetchQuestions = async id => {
+export const fetchQuestions = async (id) => {
   const q = query(collection(firebaseFirestore, 'questions'), where('sub_quiz_id', '==', id))
   const snapshot = await getDocs(q)
   return sort(snapshot.docs)
--- app/api/index.js
+++ app/api/index.js
@@ -1,30 +1,24 @@
-// import * as SQLite from "expo-sqlite";
-
-// const openDatabase = () => {
-//   if (Platform.OS === "web") {
-//     return {
-//       transaction: () => {
-//         return {
-//           executeSql: () => {},
-//         };
-//       },
-//     };
-//   }
-
-//   const db = SQLite.openDatabase("db.db");
-//   return db;
-// }
-
-// const db = openDatabase();
-
+// import 'expo-firestore-offline-persistence'
 import { getAuth } from 'firebase/auth'
 import { getDatabase } from 'firebase/database'
 import { getFirestore, initializeFirestore } from 'firebase/firestore'
-import { initializeApp } from 'firebase/app'
+import firebase from 'firebase/compat/app'
+import 'firebase/compat/firestore'
 
 import firebaseConfig from '../config/firebase'
 
-const app = initializeApp(firebaseConfig)
+let app
+if (firebase.apps.length === 0) {
+  app = firebase.initializeApp(firebaseConfig)
+} else {
+  app = firebase.app()
+}
+
+const db = app.firestore()
+db.enablePersistence()
+  .then((data) => console.log('Offline successfully enable: ', data))
+  .catch((error) => console.log('Offline error: ', error))
+
 initializeFirestore(app, { useFetchStraems: false, experimentalForceLongPolling: true })
 
 const firebaseAuth = getAuth(app)
